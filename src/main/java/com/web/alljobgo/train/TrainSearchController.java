0c9e38a94522e3664fb42cf432f60e154020c2ce
rnrud <rnrudxo2872@naver.com>
2021-07-30 20:17:58 +0900

    실행시 에러 수정, master branch에 릴리즈

--- src/main/java/com/web/alljobgo/train/TrainSearchController.java
+++ src/main/java/com/web/alljobgo/train/TrainSearchController.java
@@ -3,6 +3,7 @@ package com.web.alljobgo.train;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,9 +15,10 @@ import com.web.alljobgo.train.service.HrdSearchService;
 public class TrainSearchController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(TrainSearchController.class);
+	private final HrdSearchService hrdSearchService;
 	
 	public TrainSearchController(HrdSearchService hrdSearchService) {
-		// TODO Auto-generated constructor stub
+		this.hrdSearchService = hrdSearchService;
 	}
 	
 	@GetMapping(value = "")
@@ -26,8 +28,8 @@ public class TrainSearchController {
 	}
 	
 	@GetMapping(value="/api/hrd")
-	public String getAPIData(SearchVO searchVO) {
-		
-		
+	public String getAPIData(Model model, SearchVO searchVO) {
+		model.addAttribute(hrdSearchService.search(searchVO));
+		return "search/result";
 	}
 }
